bf8e7a119d92e0669bc221931cbebd576730b46f
Lore <lorena.palominos@gmail.com>
2018-03-20 14:08:49 -0300

    avance

--- static/js/apps/main.jsx
+++ static/js/apps/main.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = ({ message }) => <div>{ message }</div>;
+import { catalog } from './../../../data/mock'; 
 
-ReactDOM.render(<App message="hello world" />, document.getElementById('app'));
+const Items = ({ imageURL, id, name, price, currency }) => 
+    catalog.map((data =>
+    <div className="container">
+    <img src={data.imageURL}/>
+    <p>{data.name}</p>
+    <p>{data.currency}{data.price}</p>
+    <button id="add">Agregar Al Carro</button>
+    </div> 
+    ))
+const App = ({ message }) => <div className="message">{ message }</div>;
+
+ReactDOM.render(<App message="TOTAL:" />, document.getElementById('app'));
+ReactDOM.render(<Items/>, document.getElementById('products'));
